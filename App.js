3d5b0f280a588e5489fcd2ebf8fc91c05fe000e7
SantamRC <santamdev404@gmail.com>
2021-11-06 14:04:26 +0530

    Removed some unwanted packages

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import codePush from "react-native-code-push";
-
 
 let codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_RESUME };
 
 function App() {
+  useEffect(() => {
+    codePush.sync(
+      { installMode: codePush.InstallMode.IMMEDIATE },
+      syncWithCodePush,
+      null
+    );
+  });
+
+  const syncWithCodePush = (status) => {
+    console.log(status);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Mediwell App Testing</Text>
-      <StatusBar style="auto" />
+      <Text>Mediwell New Update</Text>
     </View>
   );
 }
@@ -24,6 +33,4 @@ const styles = StyleSheet.create({
   },
 });
 
-
 export default codePush(codePushOptions)(App);
-//export default App;
