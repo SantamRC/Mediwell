96038baea3cbdd58ba505d03629b73846ddec2db
SantamRC <santamdev404@gmail.com>
2021-11-15 20:28:14 +0530

    Removed all firebase dependencies

--- Screens/Startup/Startup.jsx
+++ Screens/Startup/Startup.jsx
@@ -1,43 +1,14 @@
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import {
-  GoogleSignin,
-  GoogleSigninButton,
-  statusCodes,
-} from "@react-native-google-signin/google-signin";
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 const Startup = () => {
-  const login = () => {
-    GoogleSignin.signIn()
-      .then((data) => {
-        console.log(data.user);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
-
-  useEffect(() => {
-    GoogleSignin.configure({
-      webClientId: "AIzaSyAl6tJk5KJc81Av5KoUteKJB1K3OCZyo7w",
-      offlineAccess: true,
-      hostedDomain: "",
-      forceConsentPrompt: true,
-    });
-  });
-
   return (
-    <View style={styles.basic}>
-      <Text>Mediwell</Text>
-      <GoogleSigninButton onPress={login} />
+    <View>
+      <Text>Startup</Text>
     </View>
-  );
-};
+  )
+}
 
-export default Startup;
+export default Startup
 
-const styles = StyleSheet.create({
-  basic: {
-    alignItems: "center",
-  },
-});
+const styles = StyleSheet.create({})
